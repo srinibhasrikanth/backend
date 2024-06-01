04ff40b361731b0afa462376f70c5bcef404480a
srinibhasrikanth <srinibha.srikanth@gmail.com>
2024-06-01 15:06:47 +0530

    build

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
 const { getAllEvents } = require("./controller/eventController.js");
-const eventModel=require('./model/eventModel.js')
+const eventModel = require("./model/eventModel.js");
 //dotenv config
 dotenv.config();
 
@@ -25,19 +25,7 @@ app.get("/api/v1/events", (req, res) => {
   res.json("Hello welcome to my world");
 });
 
-app.use("/api/v1/events/get-all-events", async (req, res) => {
-  try {
-    const events = await eventModel.find({});
-    res.status(200).json(events);
-  } catch (error) {
-    console.error("Error fetching events:", error);
-    res.status(500).json({
-      success: false,
-      message: "Error fetching events",
-      error: error.message,
-    });
-  }
-});
+app.use("/api/v1/events/get-all-events", getAllEvents);
 
 app.use("/api/v1/auth", require("./route/authRoute.js"));
 
