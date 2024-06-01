d4bf558fb02f5973d8f30df491beb20e8cda49c0
srinibhasrikanth <srinibha.srikanth@gmail.com>
2024-06-01 13:15:54 +0530

    hi

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("../backend/config/db.js");
+const morgan = require("morgan");
+const cors = require("cors");
+
+//dotenv config
+dotenv.config();
+
+//rest app
 const app = express();
-const PORT = 4000;
 
+//database config
+connectDB();
+
+//middlewares
+app.use(cors());
+// app.use(morgan());
+app.use(express.json());
+
+//routes
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome, your app is working well");
+  res.send("Hello welcome to my world");
 });
 
+app.use("/api/v1/events", require("./route/eventRoute.js"));
+
+app.use("/api/v1/auth", require("./route/authRoute.js"));
+
+//PORT number
+const PORT = process.env.PORT || 8080;
+
+//Listening the PORT
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server is running ${PORT}`);
 });
-
-// Export the Express API
-module.exports = app;
