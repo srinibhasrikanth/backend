77fe60d4da9df50f90d6387e1f61ff74f1716271
srinibhasrikanth <srinibha.srikanth@gmail.com>
2024-06-01 12:49:11 +0530

    added all

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("../backend/config/db.js");
+const morgan = require("morgan");
+const cors = require("cors");
 
+//dotenv config
+dotenv.config();
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-})
+//rest app
+const app = express();
 
+//database config
+connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+//middlewares
+app.use(cors());
+// app.use(morgan());
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello welcome to my world");
 });
 
-// Export the Express API
-module.exports = app
+app.use("/api/v1/events", require("./route/eventRoute.js"));
+
+app.use("/api/v1/auth", require("./route/authRoute.js"));
+
+//PORT number
+const PORT = process.env.PORT || 8080;
+
+//Listening the PORT
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
