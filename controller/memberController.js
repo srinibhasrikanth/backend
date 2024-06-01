e4fd114d8bf6bd2524882e787f3c389c3d8de7e1
srinibhasrikanth <srinibha.srikanth@gmail.com>
2024-06-01 17:53:25 +0530

    save all core

--- controller/memberController.js
+++ controller/memberController.js
@@ -1,10 +1,17 @@
 const coreModel = require("../model/coreModel");
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 const saveCore = async (req, res) => {
   try {
-    const coreData = req.body;
-    const newData = new coreModel(coreData);
-    const saveData = await newData.insertMany();
+    const coreDataArray = req.body; // Assuming req.body is an array of objects
+
+    // Use insertMany to save an array of documents
+    const savedData = await coreModel.insertMany(coreDataArray);
+
+    res.status(200).json({
+      success: "true",
+      message: "Sucessfully saved",
+      savedData,
+    });
   } catch (error) {
     res.status(500).json({
       success: "false",
