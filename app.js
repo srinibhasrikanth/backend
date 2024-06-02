ad67ffa8892ab6df241bfe1877b7ce035e4b9c94
srinibhasrikanth <srinibha.srikanth@gmail.com>
2024-06-02 21:03:05 +0530

    new controllers file

--- app.js
+++ app.js
@@ -19,14 +19,16 @@ const app = express();
 connectDB();
 
 //middlewares
-app.use(
-  cors({
-    // origin: ["https://frontend-nu-flame-39.vercel.app"],
-    origin: ["http://localhost:3000"],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     // origin: ["https://frontend-nu-flame-39.vercel.app"],
+//     origin: ["http://localhost:3000"],
+//     methods: ["POST", "GET", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 app.use(express.json());
 
 app.post("/send-emails", async (req, res) => {
